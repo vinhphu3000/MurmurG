8218a33b6dea0041a062ec22753978271b9f85dd
reillydonovan <reillydonovan3@gmail.com>
2017-05-29 00:30:09 -0700

    Added UI Canvas Test

---
+++ Assets/Scripts/RectAnim.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using HoloToolkit.Unity.InputModule;
+
+
+public class RectAnim : MonoBehaviour
+{
+
+    Animator anim;
+    // Use this for initialization
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!anim.GetBool("idle"))
+        {
+            anim.Play("RectMove");
+
+        }
+        else if (anim.GetBool("idle"))
+        {
+
+            anim.Play("RectIdle");
+        }
+
+    }
+
+    void OnSelect()
+    {
+
+
+        if (anim.GetBool("idle"))
+        {
+
+            anim.SetBool("idle", false);
+        }
+        else
+        {
+            anim.SetBool("idle", true);
+
+        }
+
+
+    }
+
+    void OnMouseDown()
+    {
+
+        OnSelect();
+    }
+
+}
---
+++ Assets/Scripts/ToggleObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleObject : MonoBehaviour {
+    public bool Toggle;
+    public GameObject ToggleTarget;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Toggle)
+        {
+            ToggleTarget.gameObject.SetActive(true);
+        }
+        else
+        {
+            ToggleTarget.gameObject.SetActive(false);
+        }
+	}
+
+    public void ToggleMe()
+    {
+        Toggle = !Toggle;
+    }
+}
