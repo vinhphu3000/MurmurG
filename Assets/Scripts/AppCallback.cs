49bde77e48da9df72240b743afb014575368bc34
reillydonovan <reillydonovan3@gmail.com>
2017-05-30 10:38:04 -0700

    Added AppCallback

---
+++ Assets/Scripts/AppCallback.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppCallback : MonoBehaviour
+{
+    public TextMesh debugTextMesh;
+    public string output = "";
+    public string stack = "";
+    void OnEnable()
+    {
+        Application.logMessageReceived += HandleLog;
+    }
+    void OnDisable()
+    {
+        Application.logMessageReceived -= HandleLog;
+    }
+    void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        output = logString;
+        stack = stackTrace;
+        debugTextMesh.text = logString;
+    }
+    
+
+}
