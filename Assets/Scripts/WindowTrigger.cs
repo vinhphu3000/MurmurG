cc3ac57983c95a615d1c05014fc47b6a10dea968
reillydonovan <reillydonovan3@gmail.com>
2017-08-03 09:15:59 -0700

    corrected window & render order

---
+++ Assets/Scripts/WindowTrigger.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowTrigger : MonoBehaviour
+{
+    
+    public GameObject TargetGameObject;
+   
+
+    void Start()
+    {
+      //  rend = ChangeObject.GetComponent<Renderer>();
+      //  rend.enabled = true;
+     //   rend.sharedMaterial = material[0];
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        switch (col.tag)
+        {
+            case "MainCamera":
+                //     Debug.Log("Object entered the trigger");
+                TargetGameObject.SetActive(false);
+                break;
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        switch (col.tag)
+        {
+            case "MainCamera":
+               // Debug.Log("Object is within the trigger");
+                break;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        switch (col.tag)
+        {
+            case "MainCamera":
+                //  Debug.Log("Object exited the trigger");
+                //   rend.sharedMaterial = material[0];
+                TargetGameObject.SetActive(true);
+                break;
+        }
+    }
+}
