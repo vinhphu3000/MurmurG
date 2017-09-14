10cca0b1c664ce9c4499ee78e7d38e7e93afe1f6
reillydonovan <reillydonovan3@gmail.com>
2017-09-14 09:30:47 -0700

    getting ready for press

---
+++ Assets/Scripts/TogglePingo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TogglePingo : MonoBehaviour
+{
+
+    public GameObject togglePingo;
+    bool toggle;
+
+    void Start()
+    {
+
+        toggle = false;
+    }
+    // Use this for initialization
+    public void togglePingoObject()
+    {
+        toggle = !toggle;
+        togglePingo.SetActive(toggle);
+
+    }
+}
