6d3b6ac1179738495e679f0726821ece7dca27b8
reillydonovan <reillydonovan3@gmail.com>
2017-09-14 06:47:47 -0700

    cleaned up all tiles except the 4th, solved render issue

---
+++ Assets/Scripts/ShowMesh.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class ShowMesh : MonoBehaviour {
+
+	public GameObject[] meshObject;
+	bool toggle;
+
+	void Start()
+	{
+
+		toggle = false;
+	}
+	// Use this for initialization
+	public void MeshToShow () {
+		for (int i = 0; i < meshObject.Length; i++)
+		{
+			toggle = !toggle;
+			meshObject[i].GetComponent<MeshRenderer>().enabled = toggle;
+		}
+
+	}
+}
---
+++ Assets/Scripts/ToggleGaze.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class ToggleGaze : MonoBehaviour {
+
+	public GameObject[] toggleTapGameObject;
+	bool toggle;
+
+	void Start()
+	{
+
+		toggle = false;
+	}
+	// Use this for initialization
+	public void ObjectToToggle () {
+		for (int i = 0; i < toggleTapGameObject.Length; i++)
+		{
+			toggle = !toggle;
+			toggleTapGameObject[i].GetComponent<TapToPlace>().enabled = toggle;
+		}
+
+	}
+}
