ca2d6c1195035078873f4eb23dd5c0674f0d8f4b
reillydonovan <reillydonovan3@gmail.com>
2017-10-11 18:34:50 -0700

    Getting Mystic Demo Ready
    still need to adjust y coordinate for reset, perhaps add move up / down / left / right / rotate script

--- Assets/Scripts/ResetObject.cs
+++ Assets/Scripts/ResetObject.cs
@@ -18,7 +18,7 @@ public class ResetObject : MonoBehaviour {
     public void ResetTransform()
     {
         toggle = !toggle;
-        resetObject.transform.position = new Vector3(resetTargetObject.transform.position.x, resetObject.transform.position.y, resetTargetObject.transform.position.z - 2.0f);
+        resetObject.transform.position = new Vector3(resetTargetObject.transform.position.x, resetObject.transform.position.y, resetTargetObject.transform.position.z);
 
     }
 }
